74fa3bf8a4a512999715acbe3609c2e37aa21baa
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-05-18 12:14:42 +0930

    teapot++

--- loader.js
+++ loader.js
@@ -59,7 +59,7 @@ function parseObj(objFile) {
 
 
 function onLoad() {
-    request('GET', './objects/bunny.obj', function (xhr) {
+    request('GET', './objects/teapot.obj', function (xhr) {
         var model = parseObj(xhr.responseText);
         start({ 'bunny' : model });
     });
